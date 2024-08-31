e6fdb6d7308ec7311efc04393cd4a14b5bffa7c1
Ruturaj Chandgude <[chandguderuturaj@gmail.com]>
2024-08-31 12:09:54 +0530

    chatpage changes

--- frontend/src/Pages/Chatpage.js
+++ frontend/src/Pages/Chatpage.js
@@ -1,6 +1,9 @@
 import { Box } from "@chakra-ui/layout";
 import { ChatState } from "../Context/ChatProvider"
 import SideDrawer from "../components/miscellaneous/SideDrawer"
+import Chatbox from "../components/Chatbox";
+import MyChats from "../components/MyChats";
+
 
 const Chatpage = () => {
   const {user}=ChatState()
